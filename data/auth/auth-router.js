352304ba783f25a42846f69b2bb6408cb820d4ef
Evans Ibok <evansibok@gmail.com>
2020-01-29 15:13:45 +0100

    Hash user password on registration

--- data/auth/auth-router.js
+++ data/auth/auth-router.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const validatePostBody = require('../middlewares/validatePostBody')
 const UsersDb = require('../users/users-model');
 
 router.post('/register', validatePostBody, (req, res) => {
   let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
   
   UsersDb.addUser(user)
-    .then(user => {
-      res.status(201).json(user);
+    .then(savedUser => {
+      res.status(201).json(savedUser);
     })
     .catch(err => {
       res.status(500).json({
